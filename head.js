ce84902cac589e976384fdf26500769544fb18bf
Cody <codylau6233@hotmail.com>
2023-10-18 13:26:12 -0700

    added AI suggestion to check if input is an array

--- head.js
+++ head.js
@@ -7,11 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(array) {
+  if (!Array.isArray(array)) {
+    console.log('Input is not an array');
+    return;
+  }
   return array[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([4]), 4);
 assertEqual(head([]), undefined);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["Hi"]), "Hi");
+assertEqual(head("Hi"), "Hi");

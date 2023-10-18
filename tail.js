7a520e813cf500078d61d5f59e83bc5668839dae
Cody <codylau6233@hotmail.com>
2023-10-18 13:52:08 -0700

    added tail function to library

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const tail = function(array) {
+  if (!Array.isArray(array)) {
+    console.log('Input is not an array');
+    return;
+  }
+  return array.slice(1);
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const result = tail(words);
+tail(words);
+assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");

1c41665a159aca854547a9d0f697147a92a5ad25
Cody <codylau6233@hotmail.com>
2023-10-22 13:20:04 -0700

    Minor edit on test cases

--- countLetters.js
+++ countLetters.js
@@ -20,7 +20,6 @@ const countLetters = function(sentence) {
   // Use .replaceAll method to remove all white space in sentence
   const sentenceWithoutSpace = sentence.replaceAll(' ', '');
 
-  //for loop to iterate the sentence string
   for (const letter of sentenceWithoutSpace) {
 
     // if countObject[letter] is not undefined, have a truthy value, then add 1 count to the value
@@ -45,9 +44,7 @@ const convertObjToStr = function(Obj) {
   return result;
 };
 
-console.log((convertObjToStr(countLetters("lighthouse in the house"))))
-
-// Test Case 1: function should return expected string
+// Test Case 1: function should return expected object string 
 assertEqual(convertObjToStr(countLetters("lighthouse in the house")), "l:1 i:2 g:1 h:4 t:2 o:2 u:2 s:2 e:3 n:1 ");
-// Test Case 2:
-assertEqual(convertObjToStr(countLetters("LHL")), "L:2 H:1 ");
+// Test Case 2: function should return 1, there's only 1 H in the input argument
+assertEqual(countLetters("LHL")["H"], 1);

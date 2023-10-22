b3eabbeeb7f0f6d2b0c3c4456b6e8fe6d7d9442f
Cody <codylau6233@hotmail.com>
2023-10-22 14:30:37 -0700

    Add more test cases to letterPosition.js

--- letterPositions.js
+++ letterPositions.js
@@ -27,15 +27,15 @@ const letterPositions = function(sentence) {
   
     const letter = sentence[i];
 
-    // if there's a blank space, skip to the next index 
-    if (letter === ' ') {
-      continue;
-    } else if (!results[letter]) {
-      // If letter is not in the result property, assign that letter index in an array
-      results[letter] = [i];
-    } else {
-      // If letter is already in the result property, push and index to the pre-exist array
-      results[letter].push(i);
+    // if there's no blank space then
+    if (letter !== ' ') {
+      if (!results[letter]) {
+        // If letter is not in the result property, assign that letter index in an array
+        results[letter] = [i];
+      } else {
+        // If letter is already in the result property, push and index to the pre-exist array
+        results[letter].push(i);
+      }
     }
   }
   return results;
@@ -47,3 +47,5 @@ assertArraysEqual(letterPositions("hello")["l"], [2, 3]);
 assertArraysEqual(letterPositions("hello")["h"], [0]);
 // Test Case 3; object property of "h" should have a value of [5], blank-space should not be counted
 assertArraysEqual(letterPositions("hello world")["w"], [6]);
+assertArraysEqual(letterPositions("hello wor,ld")[","], [9]);
+assertArraysEqual(letterPositions("abcdefg")["f"], [5]);

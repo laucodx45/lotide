eab7b5a51dde80b2cbc601f00a07fb7f0173b4d9
Cody <codylau6233@hotmail.com>
2023-10-22 22:57:23 -0700

    Fix bug in eqObjects function, utilize eqArray function

--- eqObjects.js
+++ eqObjects.js
@@ -1,23 +1,21 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
-
   if (array1.length !== array2.length) {
     return false;
   }
-
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
   return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
 };
 
 /*Implement the definition for function eqObjects which will take in two objects
@@ -45,7 +43,9 @@ const eqObjects = function(object1, object2) {
     return false;
   } else {
     for (const key of keyOfObj1) {
-      if (!object2[key]) {
+      if (Array.isArray(object1[key])) {
+        return eqArrays(object1[key], object2[key]);
+      } else if (object1[key] !== object2[key]) {
         return false;
       }
     }

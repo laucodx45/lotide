b794525263791d501562bf99330c570c53580251
Cody <codylau6233@hotmail.com>
2023-10-22 21:49:15 -0700

    Add object.key() method and repace for in loop with for of loop in findKeyByValue function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -11,12 +11,16 @@ const assertEqual = function(actual, expected) {
  * @param {*} object
  * @param {*} value
  * process
- * for in loop to iterate objects
+ * Use Object.keys() to obtain the array containing each key in object
+ * Use a for of loop to iterate the keys of object
+ * if the value called using key equals to the input value argument
  * @returns key that contains the input value
  */
 const findKeyByValue = function(object, value) {
 
-  for (const key in object) {
+  const keys = Object.keys(object);
+
+  for (const key of keys) {
     if (object[key] === value) {
       return key;
     }
@@ -24,11 +28,13 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
+// Test Case 1: function should return key "drama"
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// Test Case 2: function should return undefined, "That '70s Show" is not a value input object argument
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

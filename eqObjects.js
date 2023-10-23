e101fa807d8fb8b9c2e411906d165c0a0bc6d860
Cody <codylau6233@hotmail.com>
2023-10-22 23:28:06 -0700

    Add more comments to explain what the code does in eqObject function

--- eqObjects.js
+++ eqObjects.js
@@ -30,26 +30,29 @@ Two objects are equal when:
  * @param {*} object1
  * @param {*} object2
  * process
- * Check if they have the same number of keys, compare the length of the keys array
+ * Return true if they have the same number of keys, compare the length of the keys array
  * Use a for of loop, if using key of object1 not return truthy then the two object have different value for the key
  * @returns boolean
  */
 
 const eqObjects = function(object1, object2) {
+  // Keys of the object in arguments in array
   const keyOfObj1 = Object.keys(object1);
   const keyOfObj2 = Object.keys(object2);
 
   if (keyOfObj1.length !== keyOfObj2.length) {
     return false;
-  } else {
-    for (const key of keyOfObj1) {
-      if (Array.isArray(object1[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]) {
+  }
+
+  for (const key of keyOfObj1) {
+    // Check if the value is an array
+    if (Array.isArray(object1[key])) {
+      // if the value array is not equal, return false
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
-      }
+      } // if the value of the key in obj1 is not equal to obj2 return false
+    } else if (object1[key] !== object2[key]) {
+      return false;
     }
   }
   return true;

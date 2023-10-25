2dc0d22a49017fcb0589e7ed4457e35cca87e79e
Cody <codylau6233@hotmail.com>
2023-10-24 20:18:00 -0700

    Add findKey.js, function findKey scan object argument and return the first key for which the callback argument returns a truthy value, function passes assertEqaul test.

---
+++ findKey.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+/**
+ * findKey should scan the object and return the first key for which the callback returns a truthy value
+ * If no key is found, then it should return undefined
+ * @param {*} object
+ * @param {*} callback
+ * process
+ * Add for in loop to iterate object
+ * Add if statemen inside loop to check whether passing object key into the callback argument returns a truthy value
+ * If it returns a truthy value, return the key
+ */
+
+const findKey = (object, callback) => {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// Test Case 1: function should return key "noma"
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+// Test Case 2: functio should return key "Akaleri"
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
+
+
+

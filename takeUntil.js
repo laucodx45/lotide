be65c4c9ef50516f97357bb1ea94cc12394ee4d3
Cody <codylau6233@hotmail.com>
2023-10-24 18:26:44 -0700

    Remove [] at line 35 in takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -32,7 +32,7 @@ const takeUntil = function(array, callback) {
   // ...
   for (let i = 0; i < array.length; i++) {
     if (callback(array[i])) {
-      return array.slice([0], [i]);
+      return array.slice(0, i);
     }
   }
 };

ccd760162cddc3e32bfde1de918958620c726f47
Cody <codylau6233@hotmail.com>
2023-11-01 12:18:50 -0700

    Install mocha and chai package, add node_module to .gitignore file, refactor headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,29 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([4]), 4);
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hi"]), "Hi");
-assertEqual(head("Hi"), "Hi");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("return undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns Hello for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("should return \"Hi\" for [\"Hi\"]", () => {
+    assert.strictEqual(head(["Hi"]), "Hi");
+  });
+
+  it("should return undefined for \"Hi\"", () => {
+    assert.strictEqual(head("Hi"), undefined);
+  });
+});

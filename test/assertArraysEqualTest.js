e73ff79fb2bb48a48588c5dd201262b48e06195d
Cody <codylau6233@hotmail.com>
2023-10-31 23:51:49 -0700

    Add comments in assertArraysEqualTest.js

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,7 +2,7 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1,2,3], [1,2,3]); // should pass
 assertArraysEqual([1,2,3], [1,2]); // should fail
-assertArraysEqual([], []); // should pass
-assertArraysEqual(['hi', 'bye'], [2, 3]); // should fail
+assertArraysEqual([], []); // should pass, both arrays are empty
+assertArraysEqual(['hi', 'bye'], [2, 3]); // should fail, these arrays have the same length but different values
 assertArraysEqual(['hi', 'bye'], ['hi', 'bye']); // should pass
-assertArraysEqual(['hi', 'bye'], ['bye', 'hi']); // should fail
+assertArraysEqual(['hi', 'bye'], ['bye', 'hi']); // Expecting this test to fail due to the order of elements

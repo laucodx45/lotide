c9e7b86818fe3e0bc7db5883de2dbcd050e8538b
Cody <codylau6233@hotmail.com>
2023-11-03 16:37:20 -0700

    Update documentation in head.js

--- head.js
+++ head.js
@@ -1,3 +1,8 @@
+/**
+ * head takes in an array and returns the first element in the array.
+ * @param {array} array
+ * @returns first element in the array.
+ */
 const head = (array) => {
   if (!Array.isArray(array)) {
     console.log('Input is not an array');

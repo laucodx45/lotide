196161a9b8ac24e28a7300c51f5a7aff9a2cfa0c
Cody <codylau6233@hotmail.com>
2023-11-02 22:57:51 -0700

    Refactor eqArraysTest.js: use mocha test and chai assertion library for unit testing

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,26 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+const {eqArrays} = require('../index');
 
-assertEqual(eqArrays([1, 2, 3], [1, 3, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([], [1, 2, 3]), false);
-assertEqual(eqArrays([], []), true);
-// if input argument is not an array, function should return false;
-assertEqual(eqArrays("hi", [1, 2, 3]), false);
+describe("#eqArrays", () => {
+
+  it("returns false for [1, 2, 3] and [1, 3, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 3, 3]), false);
+  });
+
+  it("returns true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false for [] and [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([], [1 , 2, 3]), false);
+  });
+
+  it("returns true for [] and []", () => {
+    assert.strictEqual(eqArrays([], []), true);
+  });
+
+  it("returns false if one of the argument is not an array", () => {
+    assert.strictEqual(eqArrays([], "hi"), false);
+  });
+
+});

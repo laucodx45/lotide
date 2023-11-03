4b345709d186a75d9263731a4205021cab77764f
Cody <codylau6233@hotmail.com>
2023-11-03 10:51:18 -0700

    Refactor eqObjectsTest.js: remove comments

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -18,10 +18,3 @@ describe("#eqObjects", () => {
   });
 
 });
-// // Test code
-// 
-// const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-// assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true); // => true
-
-// const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-// assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
